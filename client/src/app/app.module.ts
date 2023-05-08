5816d6e01462806ff8f67949d5780167bd8f5784
Evelina Aleksandrova <evety.1dtvd@gmail.com>
2023-05-08 23:23:02 +0300

    Initial commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,23 @@
+/* eslint-disable  */
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-create/post-list/post-list.component';
+
+@NgModule({
+  declarations: [AppComponent, PostCreateComponent, HeaderComponent, PostListComponent],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
