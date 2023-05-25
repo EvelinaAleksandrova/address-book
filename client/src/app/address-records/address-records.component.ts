814a20af162fd0759a64f6bdb336be5e40dc8c3a
Evelina Aleksandrova <evety.1dtvd@gmail.com>
2023-05-25 21:14:09 +0300

    new navigation

---
+++ client/src/app/address-records/address-records.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address-records',
+  templateUrl: './address-records.component.html',
+  styleUrls: ['./address-records.component.css']
+})
+export class AddressRecordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,14 +9,28 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
 import { HttpClientModule } from '@angular/common/http';
 
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { RouterModule } from '@angular/router';
+import { AddressRecordsComponent } from './address-records/address-records.component';
+import { AuthComponent } from './auth/auth.component';
+import { LabelsComponent } from './labels/labels.component';
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent, HeaderComponent, PostListComponent],
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
+    AddressRecordsComponent,
+    AuthComponent,
+    LabelsComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
@@ -25,7 +39,14 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    HttpClientModule
+    MatMenuModule,
+    MatListModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: AddressRecordsComponent },
+      { path: 'contacts', component: AddressRecordsComponent },
+      { path: 'labels', component: LabelsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
